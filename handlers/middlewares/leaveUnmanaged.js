35f1748709c2b8fd13a86e032681aa5abc699496
JinsoRaj <48287817+JinsoRaj@users.noreply.github.com>
2019-11-05 21:15:48 +0530

    Update leaveUnmanaged.js

--- handlers/middlewares/leaveUnmanaged.js
+++ handlers/middlewares/leaveUnmanaged.js
@@ -5,7 +5,7 @@ const { logError } = require('../../utils/log');
 
 const { managesGroup } = require('../../stores/group');
 
-const { chats = {-1001403950143} } = require('../../config');
+const { chats = {} } = require('../../config');
 
 const pkg = require('../../package.json');
 
